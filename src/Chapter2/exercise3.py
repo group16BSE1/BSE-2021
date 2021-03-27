d8914b07122c813ffe266598c71cf23d51e0bade
group16BSE1 <2020bse014@std.must.ac.ug>
2021-03-27 12:06:21 +0300

    Added files for Chapter 2 exercise

---
+++ src/Chapter2/exercise3.py
@@ -0,0 +1,6 @@
+hours_worked = input('Enter Hours: ')
+hours = int(hours_worked) #changing hours_worked into integer type
+rate_pay = input('Enter Rate: ')
+rate = float(rate_pay) #changing rate_pay into integer type
+gross_pay = hours * rate
+print('Pay: ', gross_pay)
---
+++ src/Chapter2/exercise5.py
@@ -0,0 +1,4 @@
+celc_temp = input('Enter temp in celsius: ')
+temp = float(celc_temp)
+fahr_temp = (temp * 9/5) + 32 #formular for converting celsius to fahrenheit
+print(temp, 'C, converted to fahreheit is ', fahr_temp)

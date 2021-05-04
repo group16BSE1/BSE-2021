586a75274fb147a5131b08b0593f0f180dd1aca5
group16BSE1 <2020bse014@std.must.ac.ug>
2021-05-04 15:21:32 +0300

    Added residential customer function

--- src/Mid_Term/project.py
+++ src/Mid_Term/project.py
@@ -5,6 +5,26 @@ def differnce_check(begin_units, end_units):
     else:
         #For readings where the ending units are less than beginning units
         units_used = (1000000000 - begin_units) + end_units
-    return units_used
+    return units_used / 10
 
 
+#calculation for residential customer
+def residential_user(galon_units):
+    galon_units = galon_units
+    total = 5.00 + (0.0005 * galon_units)
+    return total
+
+
+def main():
+    while True:
+        user_code = input("ENTER CUSTOMER CODE: ").lower()
+        if user_code in ['r', 'c', 'i']:
+            start_units = int(input("ENTER BEGINNING METER READING: "))
+            end_units = int(input("ENTER ENDING METER READING: "))
+            diff_units = differnce_check(start_units, end_units) #calculates difference of the units
+            if user_code == 'r':
+                cost_of_Units = residential_user(diff_units)
+                print(cost_of_Units)
+
+    
+main()

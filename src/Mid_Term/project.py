45910c0177a5c47a3dc192d04aa6c0f120405dc6
group16BSE1 <2020bse014@std.must.ac.ug>
2021-05-04 15:27:22 +0300

    Added commercial user function

--- src/Mid_Term/project.py
+++ src/Mid_Term/project.py
@@ -15,6 +15,16 @@ def residential_user(galon_units):
     return total
 
 
+#calculation for commercial customer
+def commercial_user(galon_units):
+    galon_units = galon_units 
+    if galon_units <= 4000000:
+        total = 1000.00
+    else:
+        total = 1000 + (0.00025 * galon_units)    
+    return total  
+
+
 def main():
     while True:
         user_code = input("ENTER CUSTOMER CODE: ").lower()
@@ -24,6 +34,9 @@ def main():
             diff_units = differnce_check(start_units, end_units) #calculates difference of the units
             if user_code == 'r':
                 cost_of_Units = residential_user(diff_units)
+                print(cost_of_Units)
+            elif user_code == 'c':
+                cost_of_Units = commercial_user(diff_units)
                 print(cost_of_Units)
 
     

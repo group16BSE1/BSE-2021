4f8ffaaacec81c946b92e8375d073d6ac4deb38a
group16BSE1 <2020bse014@std.must.ac.ug>
2021-05-04 15:14:32 +0300

    Function that calculates the difference in units

--- src/Mid_Term/project.py
+++ src/Mid_Term/project.py
@@ -1,3 +1,10 @@
-while True:
-    user_code = input('Please enter customer code: ').lower()
-    print(f'User code: {user_code}')
+#This calculates the difference in units
+def differnce_check(begin_units, end_units):
+    if end_units >= begin_units:
+        units_used = end_units - begin_units
+    else:
+        #For readings where the ending units are less than beginning units
+        units_used = (1000000000 - begin_units) + end_units
+    return units_used
+
+

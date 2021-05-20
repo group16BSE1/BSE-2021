03b80fd36f49696907695616790d0cb9d8fe0e1d
group16BSE1 <2020bse014@std.must.ac.ug>
2021-05-20 18:27:58 +0300

    Refactoring code

--- src/Project2/project2_b.py
+++ src/Project2/project2_b.py
@@ -12,26 +12,15 @@ def open_file():
     return fileHandle
 
 
-
 def process_file(file):
     while True:
-        yearInput = input('Please enter the year: ')
-        if len(yearInput) == 4:
-            try:
-                # make sure the year entered is a number
-                yearchecker = int(yearInput)
-                if yearchecker > 1979 and yearchecker < 2013:
-                    break
-                else:
-                    print('Year is not in range')
-            except:
-                print('Invalid, year should be digits')
+        yearInput = int(input('Please enter the year: '))
+        if yearInput >= 1000 and  (1979  < yearInput < 2013):
+            yearInput = str(yearInput)
+            break #Breaks if the year is correct
         else:
-            print('Year should be a 4 digit number!!')
+            print('Year is not valid!!')
 
-
-
-    #yearInput = '2000'
     while True:
         incomeSelected = input('Please select income:'
                                '\n\t 1 \t Low income'
@@ -39,37 +28,36 @@ def process_file(file):
                                '\n\t 3 \t Upper middle income'
                                '\n\t 4 \t High income\n')
         if incomeSelected == '1':
-            incomeIs = 'WB_LI'
+            incomeUserInput = 'WB_LI'
             display_name = 'Low income'
             break
         elif incomeSelected == '2':
-            incomeIs = 'WB_LMI'
+            incomeUserInput = 'WB_LMI'
             display_name = 'Lower middle income income'
             break
         elif incomeSelected == '3':
-            incomeIs = 'WB_UMI'
+            incomeUserInput = 'WB_UMI'
             display_name = 'Upper middle income'
             break
         elif incomeSelected == '4':
-            incomeIs = 'WB_HI'
+            incomeUserInput = 'WB_HI'
             display_name = 'High income'
             break
         else:
             print('You entered false value, try again')
+
     counter = 0
     vaccine_total = 0
+
     lowestPercent = None
     highestPercent = None
     countryWithHighest = None
     countryWithLowest = None
 
     for line in file:
-        if (line.find(incomeIs) != -1) and (line.find(yearInput) != -1):
-            country = line[:50]
-            country = country.strip()
-            percent_of_vac = line[58:61]
-            percent_of_vac = percent_of_vac.strip()
-            percent_of_vac = int(percent_of_vac)
+        if (line.find(incomeUserInput) != -1) and (line.find(yearInput) != -1):
+            country = line[:50].strip()
+            percent_of_vac = int(line[58:61].strip())
             # start point......getting the highest and lowest values
 
             if (lowestPercent is None) or (highestPercent is None):
@@ -90,13 +78,13 @@ def process_file(file):
     # for loop end point
 
     average_vaccine = vaccine_total / counter
-    print(f'For:'
-          f'\n\tThe year:\t {yearInput}'
-          f'\n\tIncome :\t {display_name}'
-          f'\n\n\tCount :\t {counter}'
-          f'\n\tAverage percentage:{average_vaccine}'
-          f'\n\tHighest percentage:{highestPercent}\tCountry: {countryWithHighest}'
-          f'\n\tLowest percentage:{lowestPercent}\tCountry: {countryWithLowest}')
+
+    print(f'For the year: {yearInput} and Income level: {display_name}\n')
+    print(f'Total findings: {counter} Countries')
+    print(f'Average Vaccination: {round(average_vaccine, 2)}%')
+    print(f'Highest: {highestPercent}% for: {countryWithHighest}')
+    print(f'Lowest: {lowestPercent}% for {countryWithLowest}')
+
 
 def main():
     fileOpened = open_file()

4090db0cf4b7a61c3b2bb5d8df1dd80e7adf3078
group16BSE1 <2020bse014@std.must.ac.ug>
2021-05-21 13:10:33 +0300

    Exercise 4

---
+++ src/Chapter8/exercise4.py
@@ -0,0 +1,11 @@
+#  A program that opens the file romeo.txt and reads it line by line and also splits the lines
+
+words = []
+fhand = open('romeo.txt', 'r')
+
+for line in fhand: #Iterate through the lines
+    for word in line.split(' '):
+        if word not in words:
+            words.append(word)
+
+print(sorted(words))
